b414621e955a0b4ecc55b504582a61154c432d0a
whiteSHADOW1234 <enkaihuang2021@gmail.com>
2025-01-20 01:43:29 +0800

    feat: Ensure words flipped by user exist

--- app/page.tsx
+++ app/page.tsx
@@ -1,139 +1,319 @@
-"use client"; // Ensure this is a Client Component
+"use client";
 
 import { useState, useEffect } from "react";
-import styles from "./Home.module.css"; // CSS module for styling
+import styles from "./Home.module.css";
+
+type PhoneticData = {
+  _id: string;
+  label: string;
+  text: string;
+};
+
+type SectionData = {
+  section1: string[];
+  section2: string[];
+  section3: string[];
+};
 
 export default function Home() {
-  const firstSet = "厶ㄘㄗㄖㄕㄔㄓㄒㄑㄐㄏㄎㄍㄌㄋㄊㄉㄈㄇㄆㄅ".split("");
-  const secondSet = "ㄨ一ㄩㄨ一ㄩㄨ一ㄩㄨ一ㄩㄨ一ㄩㄨ一ㄩㄨ一ㄩ".split("");
-  const thirdSet = "ㄤ ㄣ ㄢ ㄡ ㄠ ㄟ ㄞ ㄝ ㄜ ㄛ ㄚ ㄤˋ ㄣˊ ㄢˊ ㄡˋ ㄠˇ ㄟˊ ㄞˇ ㄝˋ ㄜˇ ㄥ".split(" ");
+  const [phonetics, setPhonetics] = useState<PhoneticData[]>([]);
+  const [currentSectionData, setCurrentSectionData] =
+    useState<SectionData | null>(null);
+  const [cardsPerRow, setCardsPerRow] = useState(0);
+  const [isLargeScreen, setIsLargeScreen] = useState(false);
+  const [selectedSection, setSelectedSection] = useState<number | null>(0);
+  const [glowSection, setGlowSection] = useState<number | null>(0);
+  const [flippedCards, setFlippedCards] = useState<Set<number>>(new Set());
+  const [currentWordIndex, setCurrentWordIndex] = useState(0);
+  const [allWords, setAllWords] = useState<string[]>([]);
+  const [revealedCards, setRevealedCards] = useState<Map<number, string>>(
+    new Map()
+  );
+  const [lastFlippedIndices, setLastFlippedIndices] = useState<number[]>([
+    -1, -1, -1,
+  ]);
 
-  // State for the shuffled alphabets for three sections
-  const [section1, setSection1] = useState<string[]>([]);
-  const [section2, setSection2] = useState<string[]>([]);
-  const [section3, setSection3] = useState<string[]>([]);
-
-  // Function to shuffle the alphabet
-  const shuffleFirstSet = () => {
-    const shuffled1 = [...firstSet].sort(() => Math.random() - 0.5);
-    return shuffled1;
-  };
-
-  const shuffleSecondSet = () => {
-    const shuffled2 = [...secondSet].sort(() => Math.random() - 0.5);
-    return shuffled2;
-  }
-
-  const shuffleaThirdSet = () => {
-    const shuffled3 = [...thirdSet].sort(() => Math.random() - 0.5);
-    return shuffled3;
-  }
-
-  // Effect to shuffle the alphabets for each section on component mount
   useEffect(() => {
-    setSection1(shuffleFirstSet()); // Shuffle for section 1
-    setSection2(shuffleSecondSet()); // Shuffle for section 2
-    setSection3(shuffleaThirdSet()); // Shuffle for section 3
+    const calculateCardsPerRow = () => {
+      if (typeof window !== "undefined") {
+        const screenWidth = window.innerWidth;
+        setIsLargeScreen(screenWidth > 1300);
+        // Get the container's horizontal padding and margins
+        const container = document.querySelector(`.${styles.mainContainer}`);
+        if (container) {
+          const containerStyles = window.getComputedStyle(container);
+          const containerPaddingLeft = parseFloat(containerStyles.paddingLeft);
+          const containerPaddingRight = parseFloat(
+            containerStyles.paddingRight
+          );
+          const containerMarginLeft = parseFloat(containerStyles.marginLeft);
+          const containerMarginRight = parseFloat(containerStyles.marginRight);
+
+          const totalHorizontalSpacing =
+            containerPaddingLeft +
+            containerPaddingRight +
+            containerMarginLeft +
+            containerMarginRight;
+          const availableWidth = screenWidth - totalHorizontalSpacing;
+          setCardsPerRow(
+            Math.floor(availableWidth / (isLargeScreen ? 83 : 63))
+          );
+        }
+      }
+    };
+
+    calculateCardsPerRow();
+    window.addEventListener("resize", calculateCardsPerRow);
+
+    return () => window.removeEventListener("resize", calculateCardsPerRow);
+  }, [isLargeScreen]);
+
+  useEffect(() => {
+    const fetchPhonetics = async () => {
+      try {
+        const response = await fetch("/api/bopomofo");
+        if (!response.ok) {
+          const errorData = await response.json();
+          console.error("Failed to fetch phonetic data:", errorData.error);
+          return;
+        }
+        const data = await response.json();
+        setPhonetics(data.phonetics as PhoneticData[]);
+        setGlowSection(0);
+      } catch (error) {
+        console.error("Failed to fetch phonetic data:", error);
+      }
+    };
+
+    fetchPhonetics();
   }, []);
 
+  useEffect(() => {
+    if (phonetics.length > 0) {
+      const numberOfWordsToSelect = 30;
+      const shuffled = [...phonetics].sort(() => 0.5 - Math.random());
+      const selectedWords = shuffled
+        .slice(0, numberOfWordsToSelect)
+        .map((item) => item.text);
+      setAllWords(selectedWords);
+      setFlippedCards(new Set());
+      setRevealedCards(new Map());
+      setSelectedSection(0);
+      setGlowSection(0);
+    }
+  }, [phonetics]);
+
+  useEffect(() => {
+    if (allWords.length > 0 && cardsPerRow > 0) {
+      const selectedWord = allWords[currentWordIndex % allWords.length];
+      const regex =
+        /([ㄭ|ㄅ|ㄆ|ㄇ|ㄈ|ㄉ|ㄊ|ㄋ|ㄌ|ㄍ|ㄎ|ㄏ|ㄐ|ㄑ|ㄒ|ㄓ|ㄔ|ㄕ|ㄖ|ㄗ|ㄘ|ㄙ|ㄧ|ㄨ|ㄩ|ㄚ|ㄛ|ㄜ|ㄝ|ㄞ|ㄟ|ㄠ|ㄡ|ㄢ|ㄣ|ㄤ|ㄥ|ㄦ])([ˇˋˊ˙]?)/g;
+      const parts = [];
+      let match;
+
+      while ((match = regex.exec(selectedWord)) !== null) {
+        parts.push(match[1] + (match[2] || ""));
+      }
+      if (parts.length === 3) {
+        const numberOfCards = cardsPerRow * 2;
+        setCurrentSectionData({
+          section1: Array(numberOfCards).fill(parts[0]),
+          section2: Array(numberOfCards).fill(parts[1]),
+          section3: Array(numberOfCards).fill(parts[2]),
+        });
+      } else {
+        console.error(`Word "${selectedWord}" does not have three parts`);
+      }
+    }
+  }, [allWords, cardsPerRow, currentWordIndex]);
 
   // Flashcard component
   const Flashcard = ({
     letter,
     colorClass,
+    sectionIndex,
+    index,
     onFlip,
-    isLastFlipped,
+    isFlipped,
   }: {
     letter: string;
     colorClass: string;
-    onFlip: () => void; // Add onFlip prop
-    isLastFlipped: boolean; // Add isLastFlipped prop
+    sectionIndex: number;
+    index: number;
+    onFlip: (sectionIndex: number, cardIndex: number) => void;
+    isFlipped: boolean;
   }) => {
-    const [flipped, setFlipped] = useState(false);
-  
+    const cardIndex = index + sectionIndex * cardsPerRow * 2;
+
+    const handleFlip = () => {
+      if (selectedSection === sectionIndex) {
+        onFlip(sectionIndex, index);
+      } else {
+        alert("Please select a card from the correct section.");
+      }
+    };
     return (
       <div
-        className={`${styles.card} ${flipped ? styles.flipped : ""} ${isLastFlipped ? styles.glow : ""}`} // Apply glow class if it's the last flipped card
-        onClick={() => {
-          setFlipped(!flipped);
-          onFlip(); // Call onFlip function
-        }}
+        className={`${styles.card} ${isFlipped ? styles.flipped : ""} ${
+          lastFlippedIndices[sectionIndex] === index ? styles.glow : ""
+        }`}
+        onClick={handleFlip}
       >
         <div className={`${styles.cardFront} ${colorClass}`}>
           <span className={styles.letter}></span>
         </div>
-        <div className={styles.cardBack}>
-          <span className={styles.letter}>{letter}</span>
+        <div
+          className={`${styles.cardBack} ${
+            isFlipped ? styles.cardBackFlipped : ""
+          }`}
+        >
+          <span className={styles.letter}>
+            {revealedCards.get(cardIndex) || letter}
+          </span>
         </div>
       </div>
     );
   };
-  
 
   type SectionProps = {
-    letters: string[];
+    letters: string[] | undefined;
     colorClass: string;
+    sectionIndex: number;
   };
 
-  // Section of cards
-  const Section: React.FC<SectionProps> = ({ letters, colorClass }) => {
-    const [lastFlippedIndex, setLastFlippedIndex] = useState<number | null>(null);
-  
+  const Section: React.FC<SectionProps> = ({
+    letters,
+    colorClass,
+    sectionIndex,
+  }) => {
+    const handleCardFlip = (sectionIndex: number, cardIndex: number) => {
+      const globalIndex = cardIndex + sectionIndex * cardsPerRow * 2;
+      setFlippedCards((prev) => {
+        const newFlippedCards = new Set(prev);
+        if (!newFlippedCards.has(globalIndex)) {
+          newFlippedCards.add(globalIndex);
+        }
+        return newFlippedCards;
+      });
+      setRevealedCards((prev) => {
+        const newRevealedCards = new Map(prev);
+        if (!newRevealedCards.has(globalIndex)) {
+          newRevealedCards.set(globalIndex, letters ? letters[cardIndex] : "");
+        }
+        return newRevealedCards;
+      });
+      setLastFlippedIndices((prev) => {
+        const copy = [...prev];
+        copy[sectionIndex] = cardIndex;
+        return copy;
+      });
+      setSelectedSection((prevSelectedSection) => {
+        if (prevSelectedSection === sectionIndex) {
+          return sectionIndex < 2 ? sectionIndex + 1 : 0;
+        }
+        return prevSelectedSection;
+      });
+      setGlowSection((prevGlowSection) => {
+        if (prevGlowSection === sectionIndex) {
+          return sectionIndex < 2 ? sectionIndex + 1 : 0;
+        }
+        return prevGlowSection;
+      });
+      if (sectionIndex === 2) {
+        setCurrentWordIndex((prevIndex) => prevIndex + 1);
+      }
+    };
+
     return (
       <div className={styles.section}>
-        <main className={styles.cardGrid}>
-          {letters.map((letter, index) => (
+        <main
+          className={styles.cardGrid}
+          style={{
+            gridTemplateRows: `repeat(2, ${isLargeScreen ? 60 : 55}px)`,
+          }}
+        >
+          {letters?.map((letter, index) => (
             <Flashcard
-              key={letter}
+              key={index}
               letter={letter}
               colorClass={colorClass}
-              onFlip={() => setLastFlippedIndex(index)} // Update last flipped index
-              isLastFlipped={lastFlippedIndex === index} // Check if this is the last flipped card
+              sectionIndex={sectionIndex}
+              index={index}
+              onFlip={handleCardFlip}
+              isFlipped={flippedCards.has(
+                index + sectionIndex * cardsPerRow * 2
+              )}
             />
           ))}
         </main>
       </div>
     );
   };
-  
 
-
+  const [totalCards, setTotalCards] = useState(0);
   return (
     <div className={styles.mainContainer}>
       <h1 className={styles.pageTitle}>
-        注<span className={styles.straight}>ㄓㄨ</span><span className={styles.punctuation}>ˋ</span>{'\u00A0'}
-        音<span className={styles.straight}>一ㄣ</span>{'\u00A0'}
-        符<span className={styles.straight}>ㄈㄨ</span><span className={styles.punctuation}>ˊ</span>{'\u00A0'}
-        號<span className={styles.straight}>ㄏㄠ</span><span className={styles.punctuation}>ˋ</span>{'\u00A0'}
-        字<span className={styles.straight}>ㄗ</span><span className={styles.punctuation}>ˋ</span>{'\u00A0'}
-        卡<span className={styles.straight}>ㄎㄚ</span><span className={styles.punctuation}>ˇ</span> {'\u00A0'}{'\u00A0'}
-        
+        注<span className={styles.straight}>ㄓㄨ</span>
+        <span className={styles.punctuation}>ˋ</span>
+        {"\u00A0"}音<span className={styles.straight}>一ㄣ</span>
+        {"\u00A0"}符<span className={styles.straight}>ㄈㄨ</span>
+        <span className={styles.punctuation}>ˊ</span>
+        {"\u00A0"}號<span className={styles.straight}>ㄏㄠ</span>
+        <span className={styles.punctuation}>ˋ</span>
+        {"\u00A0"}字<span className={styles.straight}>ㄗ</span>
+        <span className={styles.punctuation}>ˋ</span>
+        {"\u00A0"}卡<span className={styles.straight}>ㄎㄚ</span>
+        <span className={styles.punctuation}>ˇ</span> {"\u00A0"}
+        {"\u00A0"}
       </h1>
       <div className={styles.colorTags}>
-        <span className={`${styles.colorTag} ${styles.redBackground}`}> 
-          聲<span className={styles.straight}>ㄕㄥ</span>{'\u00A0'}  
-          符<span className={styles.straight}>ㄈㄨ</span><span className={styles.punctuation}>ˊ</span>{'\u00A0'}
+        <span
+          className={`${styles.colorTag} ${styles.redBackground} ${
+            glowSection === 0 ? styles.glow : ""
+          }`}
+        >
+          聲<span className={styles.straight}>ㄕㄥ</span>
+          {"\u00A0"}符<span className={styles.straight}>ㄈㄨ</span>
+          <span className={styles.punctuation}>ˊ</span>
+          {"\u00A0"}
         </span>
-        <span className={`${styles.colorTag} ${styles.greenBackground}`}> 
-          介<span className={styles.straight}>ㄐ一ㄝ</span><span className={styles.punctuation}>ˋ</span>{'\u00A0'}
-          音<span className={styles.straight}>一ㄣ</span>{'\u00A0'}  
+        <span
+          className={`${styles.colorTag} ${styles.greenBackground} ${
+            glowSection === 1 ? styles.glow : ""
+          }`}
+        >
+          介<span className={styles.straight}>ㄐ一ㄝ</span>
+          <span className={styles.punctuation}>ˋ</span>
+          {"\u00A0"}音<span className={styles.straight}>一ㄣ</span>
+          {"\u00A0"}
         </span>
-        <span className={`${styles.colorTag} ${styles.blueBackground}`}> 
-          韻<span className={styles.straight}>ㄩㄣ</span><span className={styles.punctuation}>ˋ</span>{'\u00A0'} 
-          符<span className={styles.straight}>ㄈㄨ</span><span className={styles.punctuation}>ˊ</span>{'\u00A0'} 
+        <span
+          className={`${styles.colorTag} ${styles.blueBackground} ${
+            glowSection === 2 ? styles.glow : ""
+          }`}
+        >
+          韻<span className={styles.straight}>ㄩㄣ</span>
+          <span className={styles.punctuation}>ˋ</span>
+          {"\u00A0"}符<span className={styles.straight}>ㄈㄨ</span>
+          <span className={styles.punctuation}>ˊ</span>
+          {"\u00A0"}
         </span>
       </div>
-      <Section 
-        letters={section1} 
+      <Section
+        letters={currentSectionData?.section1}
         colorClass={styles.redBackground}
+        sectionIndex={0}
       />
-      <Section 
-        letters={section2} 
+      <Section
+        letters={currentSectionData?.section2}
         colorClass={styles.greenBackground}
+        sectionIndex={1}
       />
-      <Section 
-        letters={section3} 
+      <Section
+        letters={currentSectionData?.section3}
         colorClass={styles.blueBackground}
+        sectionIndex={2}
       />
     </div>
   );
